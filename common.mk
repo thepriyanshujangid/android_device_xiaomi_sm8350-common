010e926acf7ac0a520e73b4929f1d648253ef961
Dhina17 <dhinalogu@gmail.com>
2025-02-06 03:00:25 +0800

    sm8350-common: Implement torch light control
    Signed-off-by: rahul <rahul@aospa.co>
    Co-Authored-By: rahul <rahul@aospa.co>
    Co-Authored-By: John Galt <7730016+realjohngalt@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -137,6 +137,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm8350)
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
